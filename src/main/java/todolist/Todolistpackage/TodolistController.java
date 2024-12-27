cf2e72fb9c2d141df148ebb44be37e17c6dd24c9
Kimmy <kimsungrak00@gmail.com>
2024-12-27 16:50:37 +0900

    각 코드 별로 무슨 기능을 하는지 한글로 추가

--- src/main/java/todolist/Todolistpackage/TodolistController.java
+++ src/main/java/todolist/Todolistpackage/TodolistController.java
@@ -15,42 +15,49 @@ public class TodolistController {
     @Autowired
     private TodolistService todolistService;
 
+    //할 일 생성 (/todos)
     @PostMapping("/todos")
     public TodolistResponseDTO createTodo(@Valid @RequestBody TodolistRequestDTO requestDTO) {
         TodolistEntity createdTodo = todolistService.createTodo(requestDTO);
         return createdTodo.toResponseDTO();
     }
 
+    //전체 할 일 목록 조회 (/todos)
     @GetMapping("/todos")
     public ResponseEntity<List<TodolistResponseDTO>> getAllTodos() {
         List<TodolistResponseDTO> responseList = todolistService.getAllTodos();
         return new ResponseEntity<>(responseList, HttpStatus.OK);
     }
 
+    //특정 그룹에 속한 할 일 목록 조회 (/todos/groups/{groupId})
     @GetMapping("/todos/groups/{groupId}")
     public ResponseEntity<List<TodolistResponseDTO>> getAllTodosByGroupId(@PathVariable Long groupId) {
         List<TodolistResponseDTO> responseList = todolistService.getAllTodosByGroupId(groupId);
         return new ResponseEntity<>(responseList, HttpStatus.OK);
     }
 
+    // 특정 리스트 검색 기능
     @GetMapping("/todos/search")
     public ResponseEntity<List<TodolistResponseDTO>> searchTodos(@RequestParam String title) {
         List<TodolistResponseDTO> responseList = todolistService.searchTodosByTitle(title);
         return new ResponseEntity<>(responseList, HttpStatus.OK);
     }
 
+    // 특정 완료 상태의 할 일 목록 조회
     @GetMapping("/todos/completed")
     public ResponseEntity<List<TodolistResponseDTO>> getTodosByCompleted(@RequestParam boolean completed) {
         List<TodolistResponseDTO> responseList = todolistService.getTodosByCompleted(completed);
         return new ResponseEntity<>(responseList, HttpStatus.OK);
     }
 
+    //오늘 생성된 할 일 목록 조회 (/todos/today)
     @GetMapping("/todos/today")
     public ResponseEntity<List<TodolistResponseDTO>> getTodosCreatedToday() {
         List<TodolistResponseDTO> responseList = todolistService.getTodosCreatedToday();
         return new ResponseEntity<>(responseList, HttpStatus.OK);
     }
 
+    //할 일 업데이트 (/todos/{id})
     @PutMapping("/todos/{id}")
     public TodolistResponseDTO updateTodo(
             @PathVariable Long id,
@@ -60,6 +67,7 @@ public class TodolistController {
         return updatedTodo.toResponseDTO();
     }
 
+    //할 일 삭제 (/todos/{id})
     @DeleteMapping("/todos/{id}")
     public ResponseEntity<Void> deleteTodoById(@PathVariable Long id) {
         todolistService.deleteTodoById(id);
