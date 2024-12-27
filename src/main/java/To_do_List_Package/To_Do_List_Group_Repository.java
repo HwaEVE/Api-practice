b0983ba415b8a2faad370f9f321d9c94b35feba7
Kimmy <kimsungrak00@gmail.com>
2024-12-27 12:42:04 +0900

    삭제된 파일3

--- src/main/java/To_do_List_Package/To_Do_List_Group_Repository.java
+++
@@ -1,9 +0,0 @@
-package To_do_List_Package;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface To_Do_List_Group_Repository extends JpaRepository<To_Do_List_Group_Entity, Long> {
-    List<To_Do_List_Group_Entity> findByNameContaining(String name);
-}
